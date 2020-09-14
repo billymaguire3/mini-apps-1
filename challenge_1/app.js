7055b9a982090f17a24027e15d8e2f46a567fc2f
William Maguire <billymaguire3@gmail.com>
2020-09-14 14:40:04 -0400

    Built Tic Tac Toe Board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,35 @@
+// Add DOM event listener to our file
+document.addEventListener('DOMContentLoaded', () => {
+
+  const currentPlayer = 1;
+
+  let boardValues = ['', '', '', '', '', '', '', '', ''];
+
+  const handleSquareClick = (clickedSquareEvent) => {
+    alert('square clicked');
+    const clickedSquare = clickedSquareEvent.target;
+    const clickedSquareIndex = parseInt(
+      clickedSquare.getElementsByClassName('.square')
+    );
+
+    let currentBoardValue = boardValues[clickedSquareIndex]
+    if (currentBoardValue === '') {
 
 
+      if (currentPlayer === 1) {
+        document.getElementById(currentBoardValue).innerHTML = 'X';
+        currentPlayer = 0;
+      } else {
+        document.getElementById(currentBoardValue).innerHTML = 'O';
+        currentPlayer = 1;
+      }
+    }
+  }
+
+
+  // return a NodeList of all squares inside div with class 'board', with an event click listener on each square
+  document.querySelectorAll('.square').forEach(square => square.addEventListener('click', handleSquareClick));
+
+
+
+})
