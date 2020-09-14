35aebcecfd27a56c733418943235faa4eed89cf4
William Maguire <billymaguire3@gmail.com>
2020-09-14 16:58:46 -0400

    Implement condition to check if there is a value at the square already

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,35 +1,52 @@
 // Add DOM event listener to our file
 document.addEventListener('DOMContentLoaded', () => {
 
-  let currentPlayer = 1;
+  var gameState = {
+    currentPlayer: 1,
+    gameActive: true,
+  }
 
-  let boardValues = ['', '', '', '', '', '', '', '', ''];
+  let boardValues = ['', '', '',
+    '', '', '',
+    '', '', ''];
 
   const handleSquareClick = (clickedSquareEvent) => {
-    // alert('square clicked');
 
     const clickedSquare = clickedSquareEvent.target;
 
     const clickedSquareIndex = clickedSquare.id;
 
-    let currentBoardValue = boardValues[clickedSquareIndex]
+    let currentBoardValue = boardValues[clickedSquareIndex];
 
-    if (currentBoardValue === '') {
-
-      if (currentPlayer === 1) {
-        document.getElementById(clickedSquareIndex).innerHTML = 'X';
-        currentPlayer = 0;
-      } else {
-        document.getElementById(clickedSquareIndex).innerHTML = 'O';
-        currentPlayer = 1;
-      }
+    if (currentBoardValue === '' && gameState.gameActive) {
+      handleSquareChange(currentBoardValue, clickedSquareIndex, clickedSquare);
+    } else {
+      handleGameResult(gameState.gameActive);
     }
+  }
+
+  const handleSquareChange = (currentBoardValue, clickedSquareIndex, clickedSquare) => {
+    console.log(currentBoardValue);
+    console.log(clickedSquareIndex);
+    if (gameState.currentPlayer === 1) {
+      document.getElementById(clickedSquareIndex).innerHTML = 'X';
+      currentBoardValue[clickedSquareIndex] = 'X';
+      boardValues[clickedSquareIndex] = 'X';
+      gameState.currentPlayer = 0;
+    } else {
+      document.getElementById(clickedSquareIndex).innerHTML = 'O';
+      currentBoardValue[clickedSquareIndex] = 'O';
+      boardValues[clickedSquareIndex] = 'O';
+      gameState.currentPlayer = 1;
+    }
+  }
+
+  const handleGameResult = (isActive) => {
+
   }
 
 
   // return a NodeList of all squares inside div with class 'board', with an event click listener on each square
   document.querySelectorAll('.square').forEach(square => square.addEventListener('click', handleSquareClick));
-
-
 
 })
