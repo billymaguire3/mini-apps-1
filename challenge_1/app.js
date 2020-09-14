c105b83aaab8890a1d132a6281986c5a3438358a
William Maguire <billymaguire3@gmail.com>
2020-09-14 14:57:43 -0400

    Implement click function on squares to toggle between X and O

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,26 +1,26 @@
 // Add DOM event listener to our file
 document.addEventListener('DOMContentLoaded', () => {
 
-  const currentPlayer = 1;
+  let currentPlayer = 1;
 
   let boardValues = ['', '', '', '', '', '', '', '', ''];
 
   const handleSquareClick = (clickedSquareEvent) => {
-    alert('square clicked');
+    // alert('square clicked');
+
     const clickedSquare = clickedSquareEvent.target;
-    const clickedSquareIndex = parseInt(
-      clickedSquare.getElementsByClassName('.square')
-    );
+
+    const clickedSquareIndex = clickedSquare.id;
 
     let currentBoardValue = boardValues[clickedSquareIndex]
+
     if (currentBoardValue === '') {
 
-
       if (currentPlayer === 1) {
-        document.getElementById(currentBoardValue).innerHTML = 'X';
+        document.getElementById(clickedSquareIndex).innerHTML = 'X';
         currentPlayer = 0;
       } else {
-        document.getElementById(currentBoardValue).innerHTML = 'O';
+        document.getElementById(clickedSquareIndex).innerHTML = 'O';
         currentPlayer = 1;
       }
     }
