82f9ad88a3b843a8ff41cd60a67d39d47bf96e14
William Maguire <billymaguire3@gmail.com>
2020-09-14 18:51:09 -0400

    Implement reset board button to reset game

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,14 +1,14 @@
 // Add DOM event listener to our file
 document.addEventListener('DOMContentLoaded', () => {
 
+
   var gameState = {
     currentPlayer: 1,
     gameActive: true,
+    isDraw: false
   }
 
-  let boardValues = ['', '', '',
-    '', '', '',
-    '', '', ''];
+
 
   const handleSquareClick = (clickedSquareEvent) => {
 
@@ -21,32 +21,78 @@ document.addEventListener('DOMContentLoaded', () => {
     if (currentBoardValue === '' && gameState.gameActive) {
       handleSquareChange(currentBoardValue, clickedSquareIndex, clickedSquare);
     } else {
-      handleGameResult(gameState.gameActive);
+      return;
     }
+    handleGameResult(clickedSquareIndex);
   }
 
   const handleSquareChange = (currentBoardValue, clickedSquareIndex, clickedSquare) => {
-    console.log(currentBoardValue);
-    console.log(clickedSquareIndex);
     if (gameState.currentPlayer === 1) {
       document.getElementById(clickedSquareIndex).innerHTML = 'X';
-      currentBoardValue[clickedSquareIndex] = 'X';
       boardValues[clickedSquareIndex] = 'X';
       gameState.currentPlayer = 0;
     } else {
       document.getElementById(clickedSquareIndex).innerHTML = 'O';
-      currentBoardValue[clickedSquareIndex] = 'O';
       boardValues[clickedSquareIndex] = 'O';
       gameState.currentPlayer = 1;
     }
   }
+  let boardValues = ['', '', '',
+    '', '', '',
+    '', '', ''];
+// create array of possible winning indexes if the index all have the same value in them
+  const possibleWins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
-  const handleGameResult = (isActive) => {
-
+  const handleGameResult = (clickedSquareIndex) => {
+    var haveWinner = false;
+    possibleWins.forEach((winCombo) => {
+      var first = boardValues[winCombo[0]];
+      var second = boardValues[winCombo[1]];
+      var third = boardValues[winCombo[2]];
+      console.log('first: ', first);
+      console.log('second: ', second);
+      console.log('third ', third);
+      if (first === '' || second === '' || third === '') {
+        return;
+      }
+      if (first === second && second === third) {
+        haveWinner = true;
+        return;
+      }
+    })
+    if (haveWinner) {
+      currentWinner = document.getElementById(clickedSquareIndex).innerHTML;
+      gameState.gameActive = false;
+      alert(`The winner is Player ${currentWinner}!!`)
+    }
+    if (gameState.gameActive && !boardValues.includes('')) {
+      gameState.isDraw = true;
+      alert('The game has ended in a draw');
+    }
   }
 
+  const resetGame = () => {
 
+    console.log(document.getElementsByClassName('square'));
+    const elementArray = document.getElementsByClassName('square');
+    for (var index in elementArray) {
+      elementArray[index].innerHTML = '';
+    }
+    gameState.gameActive = true;
+    gameState.currentPlayer = 1;
+    return;
+  }
   // return a NodeList of all squares inside div with class 'board', with an event click listener on each square
   document.querySelectorAll('.square').forEach(square => square.addEventListener('click', handleSquareClick));
-
+  // event listener to reset game if button clicked
+  document.getElementById('myBtn').addEventListener("click", resetGame);
 })
