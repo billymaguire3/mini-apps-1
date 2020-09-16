7520bb32b5e98ffa2b7bf2d37249b6dec0b2044b
William Maguire <billymaguire3@gmail.com>
2020-09-16 15:32:29 -0400

    Implement babel loader, express, and rendering react to index.html page

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,17 @@
+// Place ALL react components into this one single file
+// importing react and react dom
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from App.jsx</h1>
+    </div>
+  )
+}
 
 
+ReactDOM.render(<App />, document.getElementById('app'));
+// export default App;
+// export default App;
